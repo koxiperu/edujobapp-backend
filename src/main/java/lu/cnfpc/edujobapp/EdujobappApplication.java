a058fe5362880956bbcb318bd854d359cc0f0352
koxiperu <koxiperu@gmail.com>
2026-01-15 14:05:35 +0100

    empty project

---
+++ src/main/java/lu/cnfpc/edujobapp/EdujobappApplication.java
@@ -0,0 +1,13 @@
+package lu.cnfpc.edujobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdujobappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdujobappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lu/cnfpc/edujobapp/EdujobappApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.cnfpc.edujobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdujobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
